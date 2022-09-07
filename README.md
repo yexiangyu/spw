a1f63296265e0e2dda57b0a5cbb2e9526478ecab
Xiangyu Ye <yexiangyu@gmail.com>
2022-09-07 20:09:30 +0800

    update build.rs

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # spw
+
 Sapera Wrapper
+
+## how to build
+
+### Step 1 build dll 
+```shell
+cd cpp
+md build
+cd build
+cmake ..
+cmake --build build --config Release
+cmake --build build --config Debug
+```
+
+### Step 2 build spw-rs
+
+```shell
+set SPW_DLL_DIR=where_dll_is_located
+cargo build
+```
--- spw-sys/build.rs
+++ spw-sys/build.rs
@@ -2,6 +2,10 @@ fn main() {
     println!("cargo:rustc-link-lib=spw");
     println!("cargo:rustc-link-search=c:\\tools\\spw");
     println!("cargo:rustc-link-search=d:\\Repo\\spw\\cpp\\build\\Release");
+    if let Ok(pth) = std::env::var("SPW_DLL_DIR")
+    {
+        println!("cargo:rustc-link-search={}", pth);
+    }
     // // Tell Cargo that if the given file changes, to rerun this build script.
     // println!("cargo:rerun-if-changed=src/hello.c");
     // // Use the `cc` crate to build a C file and statically link it.
